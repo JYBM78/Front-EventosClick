c4f3749d6b4ee95ca03779e8a9a24e3e83d4aa5b
Camilo <yarleyc.mejiab@uqvirtual.edu.co>
2025-10-13 18:04:32 -0500

    Se agrega la verificacion captcha

--- src/app/componentes/login/login.component.ts
+++ src/app/componentes/login/login.component.ts
@@ -6,20 +6,58 @@ import { LoginDTO } from '../../dto/login-dto';
 import Swal from 'sweetalert2';
 import { TokenService } from '../../servicios/token.service';
 import { RouterModule } from '@angular/router';
+import { NgxCaptchaModule } from 'ngx-captcha';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ReactiveFormsModule, CommonModule, RouterModule],  // Importa ReactiveFormsModule y CommonModule
+  imports: [ReactiveFormsModule, CommonModule, RouterModule,NgxCaptchaModule],  // Importa ReactiveFormsModule y CommonModule
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
 
   loginForm!: FormGroup;
+  captchaValido!: Boolean;
+token: string | null = null;
+siteKey: string = '';
+
+onCaptchaSuccess(token: string) {
+  this.http.post<any>('http://localhost:8080/api/general/validate', { token }).subscribe({
+    next: (data) => {
+      // data tiene la estructura { error: false, respuesta: true/false }
+      const isValid = data.respuesta;
+      this.captchaValido = isValid;
+      console.log('¿Captcha válido?:', isValid);
+
+      if (isValid) {
+        console.log(' Validación exitosa: El reCAPTCHA se verificó correctamente');
+      } else {
+        console.log(' Validación fallida: El reCAPTCHA no es válido, intenta nuevamente.');
+      }
+    },
+    error: (error) => {
+      console.error(' Error al validar el reCAPTCHA:', error);
+      console.error('Detalle del error:', error.error?.respuesta || 'No se pudo verificar el reCAPTCHA');
+    }
+  });
+}
 
 
-  constructor(private formBuilder: FormBuilder, private authService: AuthService,private tokenService: TokenService) {
+
+onCaptchaExpired() {
+  this.captchaValido = false;
+  this.token = null;
+  console.log('Captcha expiró');
+}
+onCaptchaError(error: any) {
+    console.error('Error en captcha:', error);
+  }
+
+
+
+  constructor(private formBuilder: FormBuilder, private authService: AuthService,private tokenService: TokenService,private http: HttpClient ) {
     
     this.crearFormulario();
     
